9a22a7ed66396ddc6640627e3eee53bbd56049c0
snizhana.tretiak <sn.piatash@gmail.com>
2024-10-24 10:00:58 +0300

    added divide feature

--- app.py
+++ app.py
@@ -10,10 +10,16 @@ def subtract(a, b):
    """Subtracts number B from A."""
    return a - b
 
+def divide(a, b):
+   """Divides number A by B. Handles division by zero."""
+   if b == 0:
+    return "Error: Division by zero is undefined."
+   return a / b
+
 
 if __name__ == '__main__':
    if len(sys.argv) < 4:
-       print("Usage: python app.py [add|subtract] num1 num2")
+       print("Usage: python3 app.py [add|subtract|divide] num1 num2")
        sys.exit(1)
 
 
@@ -28,8 +34,11 @@ if __name__ == '__main__':
    elif operation == 'subtract':
        result = subtract(a, b)
        print(f'{a} - {b} = {result}')
+   elif operation == 'divide':
+       result = divide(a, b)
+       print(f'{a} / {b} = {result}')    
    else:
-       print("Unknown operation. Use 'add' or 'subtract'.")
+       print("Unknown operation. Use 'add' or 'subtract' or 'divide'.")
 
 
 
